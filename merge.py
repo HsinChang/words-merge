2c8f3613669889c184f498328dce6b55dbb81b1f
Hsin Chang <zephyrfloraf@gmail.com>
2023-08-06 12:19:47 +0800

    failed first attempt with pyhton-docx

--- merge.py
+++ merge.py
@@ -1,12 +1,13 @@
 from docx import Document
 
+
 def combine_word_files(file1, file2):
     doc1 = Document(file1)
     doc2 = Document(file2)
 
     # Select specific chapters from each document
-    selected_chapters_doc1 = [doc1.paragraphs[0], doc1.paragraphs[1]]
-    selected_chapters_doc2 = [doc2.paragraphs[2], doc2.paragraphs[3]]
+    selected_chapters_doc1 = [doc1.paragraphs[0], doc1.paragraphs[100]]
+    selected_chapters_doc2 = [doc2.paragraphs[101], doc2.paragraphs[200]]
 
     # Create a new document and add the selected chapters from each document
     combined_doc = Document()
@@ -17,3 +18,5 @@ def combine_word_files(file1, file2):
 
     # Save the combined document
     combined_doc.save('combined.docx')
+
+combine_word_files('docx-dump/受理局指南.docx','docx-dump/受理局指南.docx')
